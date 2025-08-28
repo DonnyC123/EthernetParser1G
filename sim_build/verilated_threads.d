419f2a402ea72516ec10737cc914061d33b15b62
Donny Carl <donnyc12@outlook.com>
2025-08-28 14:43:08 -0500

    Removed Built Sim Files

--- sim_build/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /home/dcc3637/tools/verilator/include/verilated_threads.cpp \
- /home/dcc3637/tools/verilator/include/verilatedos.h \
- /home/dcc3637/tools/verilator/include/verilated_threads.h \
- /home/dcc3637/tools/verilator/include/verilated.h \
- /home/dcc3637/tools/verilator/include/verilated_config.h \
- /home/dcc3637/tools/verilator/include/verilated_types.h \
- /home/dcc3637/tools/verilator/include/verilated_funcs.h
